cdaacb303b54666997057d42c4bbe0e79d47cbbf
kadolfo9 <kaykyadolfo21@hotmail.com>
2026-07-16 20:22:39 -0300

    feat: added toast request packet

---
+++ src/zenith/Packets/ToastRequestPacket.cs
@@ -0,0 +1,27 @@
+﻿using Zenith.Raknet.Stream;
+
+namespace Zenith.Packets;
+
+sealed class ToastRequestPacket : DataPacket
+{
+    public override int Id => (int)ProtocolInfo.TOAST_REQUEST_PACKET;
+
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public override void Decode(ref BinaryStream stream)
+    {
+        Title = stream.ReadVarString();
+        Content = stream.ReadVarString();
+    }
+
+    public override Span<byte> Encode()
+    {
+        var writer = new BinaryStream();
+
+        writer.WriteVarString(Title);
+        writer.WriteVarString(Content);
+
+        return writer.GetBufferDisposing();
+    }
+}
