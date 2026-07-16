133506abf47e4490153792aa10414a92a946cfd1
kadolfo9 <kaykyadolfo21@hotmail.com>
2026-07-16 20:22:22 -0300

    feat: added play & stop sound packets

---
+++ src/zenith/Packets/PlaySoundPacket.cs
@@ -0,0 +1,63 @@
+﻿using Zenith.Raknet.Stream;
+
+namespace Zenith.Packets;
+
+sealed class PlaySoundPacket : DataPacket
+{
+    public override int Id => (int)ProtocolInfo.PLAY_SOUND_PACKET;
+
+    public string SoundName { get; set; }
+
+    public float PositionX { get; set; }
+    public float PositionY { get; set; }
+    public float PositionZ { get; set; }
+
+    public float Volume { get; set; }
+    public float Pitch { get; set; }
+
+    public float Loop { get; set; }
+
+    public long? ServerSoundHandle { get; set; } = null;
+
+    public override void Decode(ref BinaryStream stream)
+    {
+        SoundName = stream.ReadVarString();
+
+        PositionX = stream.ReadFloat(BinaryStream.Endianess.Little);
+        PositionY = stream.ReadFloat(BinaryStream.Endianess.Little);
+        PositionZ = stream.ReadFloat(BinaryStream.Endianess.Little);
+
+        Volume = stream.ReadFloat(BinaryStream.Endianess.Little);
+        Pitch = stream.ReadFloat(BinaryStream.Endianess.Little);
+
+        Loop = stream.ReadFloat(BinaryStream.Endianess.Little);
+
+        if (stream.ReadBool())
+            ServerSoundHandle = stream.ReadUnsignedVarLong();
+    }
+
+    public override Span<byte> Encode()
+    {
+        var writer = new BinaryStream();
+
+        writer.WriteVarString(SoundName);
+        writer.WriteFloat(PositionX);
+        writer.WriteFloat(PositionY);
+        writer.WriteFloat(PositionZ);
+        writer.WriteFloat(Volume);
+        writer.WriteFloat(Pitch);
+        writer.WriteFloat(Loop);
+
+        if (ServerSoundHandle != null)
+        {
+            writer.WriteBool(true);
+            writer.WriteUnsignedVarLong((long) ServerSoundHandle);
+        }
+        else
+        {
+            writer.WriteBool(false);
+        }
+
+        return writer.GetBufferDisposing();
+    }
+}
---
+++ src/zenith/Packets/StopSoundPacket.cs
@@ -0,0 +1,32 @@
+﻿using Zenith.Raknet.Stream;
+
+namespace Zenith.Packets;
+
+sealed class StopSoundPacket : DataPacket
+{
+    public override int Id => (int)ProtocolInfo.STOP_SOUND_PACKET;
+
+    public string SoundName { get; set; }
+
+    public bool StopAllSounds { get; set; }
+    public bool StopMusic { get; set; }
+
+    public override void Decode(ref BinaryStream stream)
+    {
+        SoundName = stream.ReadVarString();
+
+        StopAllSounds = stream.ReadBool();
+        StopMusic = stream.ReadBool();
+    }
+
+    public override Span<byte> Encode()
+    {
+        var writer = new BinaryStream();
+
+        writer.WriteVarString(SoundName);
+        writer.WriteBool(StopAllSounds);
+        writer.WriteBool(StopMusic);
+
+        return writer.GetBufferDisposing();
+    }
+}
